145307d940e24b0e4f2c29298eae930edabc6d42
Namisan <mongtamquoc2015@gmail.com>
2020-10-31 20:11:41 +0700

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import ROUTERS from './routers';
 
 function App() {
-  return (
-    <h1>App</h1>
-  );
+	return (
+		<Router>
+			<Switch>
+				{ROUTERS.map(route => (
+					<Route
+						exact
+						path={route.path}
+						component={route.component}
+					/>
+				))}
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/routers/index.js
@@ -0,0 +1,9 @@
+import HomePage from '../pages/HomePage';
+
+export default [
+	{
+		path: '/',
+		component: HomePage,
+		title: 'Avone'
+	}
+];
